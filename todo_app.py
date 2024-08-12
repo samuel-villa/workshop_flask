e8ae29945bf2bd3ecef79f6a615081f059e4d1ca
samuel-villa <ciullasamuel@gmail.com>
2024-08-12 12:31:28 +0200

    [IMP] code readability

--- todo_app.py
+++ todo_app.py
@@ -2,17 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Step 1
-# @app.route("/")
-# def homepage():
-#     return "<p>Hello, World!</p>"
-
-
-# Step 2
-# @app.route('/')
-# @app.route('/<name>')
-# def home(name=None):
-#     return render_template('main.html', person=name)
 
 items = [
     'Buy Food',
@@ -20,13 +9,11 @@ items = [
     'Watch Movie'
 ]
 
-# @app.route('/')
-# def home():
-#     return render_template('base.html')
 
 @app.route('/')
 def index():
     return render_template('items.html', items=items)
+
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
@@ -36,6 +23,7 @@ def create():
         return render_template('items.html', items=items)
     return render_template('create.html')
 
+
 @app.route('/update/<int:item_id>', methods=('GET', 'POST'))
 def update(item_id):
     if request.method == 'POST':
@@ -43,6 +31,7 @@ def update(item_id):
         items[item_id] = updated_item
         return render_template('items.html', items=items)
     return render_template('update.html', item=items[item_id], item_id=item_id)
+
 
 @app.route('/delete/<int:item_id>', methods=('GET', 'POST'))
 def delete(item_id):
