6ea98b76d3396e1b49886bf70d005e557a0dbfc0
samuel-villa <ciullasamuel@gmail.com>
2024-08-12 12:18:46 +0200

    [ADD] update and delete functions

--- todo_app.py
+++ todo_app.py
@@ -18,7 +18,7 @@ def create_database():
 def index():
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    c.execute('SELECT * FROM items')
+    c.execute('SELECT id, task FROM items')
     items = c.fetchall()
     conn.close()
     return render_template('items.html', items=items)
@@ -28,7 +28,6 @@ def index():
 def create():
     if request.method == 'POST':
         todo_item = request.form['new_item']
-
         conn = sqlite3.connect(DB_NAME)
         c = conn.cursor()
         c.execute('INSERT INTO items (task) VALUES (?)', (todo_item,))
@@ -36,6 +35,39 @@ def create():
         conn.close()
         return redirect(url_for('index'))
     return render_template('create.html')
+
+
+def get_item(id):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    item = c.execute('SELECT * FROM items WHERE id = (?)', (id,)).fetchone()
+    conn.close()
+    return item
+
+
+@app.route('/update/<int:item_id>', methods=('GET', 'POST'))
+def update(item_id):
+    item = get_item(item_id)
+    if request.method == 'POST':
+        updated_item = request.form['update_item']
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute('UPDATE items SET task=(?) WHERE id = (?)', (updated_item, item_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('update.html', item=item, item_id=item_id)
+
+
+@app.route('/delete/<int:item_id>', methods=('GET', 'POST'))
+def delete(item_id):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('DELETE FROM items WHERE id = ?', (item_id,))
+    conn.commit()
+    items = c.execute('SELECT * FROM items').fetchall()
+    conn.close()
+    return render_template('items.html', items=items)
 
 
 if __name__ == '__main__':
