a6a42f769836348d276768d0650fd5c9ae7b7efd
samuel-villa <ciullasamuel@gmail.com>
2024-08-12 11:18:54 +0200

    [ADD] update and delete functions

--- todo_app.py
+++ todo_app.py
@@ -35,3 +35,16 @@ def create():
         items.append(todo_item)
         return render_template('items.html', items=items)
     return render_template('create.html')
+
+@app.route('/update/<int:item_id>', methods=('GET', 'POST'))
+def update(item_id):
+    if request.method == 'POST':
+        updated_item = request.form['update_item']
+        items[item_id] = updated_item
+        return render_template('items.html', items=items)
+    return render_template('update.html', item=items[item_id], item_id=item_id)
+
+@app.route('/delete/<int:item_id>', methods=('GET', 'POST'))
+def delete(item_id):
+        items.pop(item_id)
+        return render_template('items.html', items=items)
